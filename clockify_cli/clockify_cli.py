9e665735686b7753bf13069dfd76eac5835e7596
Benjamin Zachariae <benjamin.zachariae@gmail.com>
2020-04-19 00:50:35 +0200

    finished update

--- clockify_cli/clockify_cli.py
+++ clockify_cli/clockify_cli.py
@@ -1,11 +1,20 @@
-import requests, json, datetime
-import os, re, keyring
+import requests, json, datetime, pytz
+import os, re
 import click
 
 ENDPOINT = "https://api.clockify.me/api/v1"
 VERBOSE = False
 config_file = os.path.expanduser("~/.clockify.cfg")
-CONFIG = {"api": "", "uid": "", "username": "", "wid": "", "workspace": "", "pid": "", "project": ""}
+CONFIG = {
+    "api": "",
+    "uid": "",
+    "username": "",
+    "wid": "",
+    "workspace": "",
+    "pid": "",
+    "project": "",
+    "timezone": "",
+}
 headers = {"X-Api-Key": None}
 
 
@@ -16,46 +25,47 @@ def get_workspaces():
     r = requests.get(f"{ENDPOINT}/workspaces", headers=headers)
     return {workspace["name"]: workspace["id"] for workspace in r.json()}
 
-
 def get_projects(workspace=None):
     workspaceId = get_workspaceId(workspace)
     r = requests.get(f"{ENDPOINT}/workspaces/{workspaceId}/projects", headers=headers)
     return {project["name"]: project["id"] for project in r.json()}
 
-
 def print_json(inputjson):
     click.echo(json.dumps(inputjson, indent=2))
 
-
-def get_time_format(time:datetime.datetime=None):
+def get_time_format(time: datetime.datetime = None):
+    timediff = pytz.timezone(CONFIG["timezone"]).utcoffset(datetime.datetime.now())
     if time:
-        return str(time.isoformat()) + "Z"
-    return str(datetime.datetime.now().isoformat()) + "Z"
-
+        return (time - timediff).strftime('%Y-%m-%dT%H:%M:%SZ')
+    return (datetime.datetime.now() - timediff).strftime('%Y-%m-%dT%H:%M:%SZ')
 
 def start_time_entry(
     workspace=None, description=None, billable="false", project=None, tags=None
 ):
     start = get_time_format()
     workspaceId = get_workspaceId(workspace)
+    projectId = get_projectId(project)
     body = {
         "start": start,
         "billable": billable,
         "description": description,
-        "projectId": project,
+        "projectId": projectId,
         "taskId": None,
         "tagIds": tags,
     }
+    if VERBOSE:
+        print('body:')
+        print(body)
     r = requests.post(
         f"{ENDPOINT}/workspaces/{workspaceId}/time-entries", headers=headers, json=body
     )
-    return r.json()
+    return r.json(), r.status_code
 
 def check_duration_format(duration: str):
     format = r"^\d{1,2}$|^\d{1,2}:\d{1,2}$|^\d{1,2}:\d{1,2}:\d{1,2}$"
     match = re.match(format, duration)
     if match:
-        hms = list(map(lambda i: int(i), duration.split(':')))
+        hms = list(map(lambda i: int(i), duration.split(":")))
         if len(hms) == 1:
             hms = [hms[0], 0, 0]
         if len(hms) == 2:
@@ -65,7 +75,9 @@ def check_duration_format(duration: str):
             quit()
         return hms
     else:
-        print("Incorrect duration format. Must be with one or two digits h/hh -> h:m:s | h:m | h")
+        print(
+            "Incorrect duration format. Must be with one or two digits h/hh -> h:m:s | h:m | h"
+        )
         quit()
 
 def add_time_entry(
@@ -94,18 +106,28 @@ def add_time_entry(
     )
     return r.json(), r.status_code, start
 
-
 def get_in_progress(workspace):
     workspaceId = get_workspaceId(workspace)
     r = requests.get(
-        f"{ENDPOINT}/workspaces/{workspaceId}/time-entries/in-progress", headers=headers
+        f"{ENDPOINT}/workspaces/{workspaceId}/user/{CONFIG['uid']}/time-entries?in-progress=true",
+        headers=headers,
     )
-    return r.json()
-
+    if r.status_code != 200:
+        click.echo(
+            f"Failed to get time entry in progress with status code {r.status_code}"
+        )
+        quit()
+    json = r.json()
+    if json == []:
+        click.echo("No entry in progress")
+        quit()
+    else:
+        return json[0]
 
 def finish_time_entry(workspace):
     current = get_in_progress(workspace)
     current_id = current["id"]
+    workspaceId = get_workspaceId()
     body = {
         "start": current["timeInterval"]["start"],
         "billable": current["billable"],
@@ -116,12 +138,11 @@ def finish_time_entry(workspace):
         "end": get_time_format(),
     }
     r = requests.put(
-        ENDPOINT + f"workspaces/{workspace}/timeEntries/{current_id}",
+        f"{ENDPOINT}/workspaces/{workspaceId}/time-entries/{current_id}",
         headers=headers,
         json=body,
     )
     return r.json()
-
 
 def get_time_entries(workspace=None):
     workspaceId = get_workspaceId(workspace)
@@ -182,11 +203,11 @@ def set_project(project=None):
 
     return project, projectId
 
-
 def get_user():
     r = requests.get(f"{ENDPOINT}/user", headers=headers)
     return r.json()
 
+# Commands
 
 @click.group()
 @click.option("--verbose", is_flag=True, help="Enable verbose output")
@@ -207,26 +228,33 @@ def cli(verbose):
         user = get_user()
         CONFIG["uid"] = user["id"]
         CONFIG["username"] = user["name"]
+        CONFIG["timezone"] = user["settings"]["timeZone"]
         with open(config_file, "w") as f:
             json.dump(CONFIG, f)
 
 
 @click.command("start", short_help="Start a new time entry")
-@click.argument("workspace")
-@click.argument("description")
+@click.option("-d", "--description", type=str, default=None, help="Entry description")
+@click.option("-w", "--workspace", "workspace", type=str, default=None)
 @click.option(
     "--billable", is_flag=True, default=False, help="Set if entry is billable"
 )
 @click.option("--project", "-p", default=None, help="Project ID")
 @click.option("--tag", "-g", multiple=True, help="Multiple tags permitted")
 def start(workspace, description, billable, project, tag):
-    ret = start_time_entry(workspace, description, billable, project, list(tag))
+    ret, status = start_time_entry(workspace, description, billable, project, list(tag))
+    if status == 201:
+        click.echo(
+            f'Started timer for {CONFIG["username"]} on Workspace: {CONFIG["workspace"]} with Project: {CONFIG["project"]}'
+        )
+    else:
+        click.echo(f"Failed to start timer with status {status}")
     if VERBOSE:
         print_json(ret)
 
 
 @click.command("finish", short_help="Finish an on-going time entry")
-@click.argument("workspace")
+@click.option("-w", "--workspace", "workspace", type=str, default=None)
 def finish(workspace):
     ret = finish_time_entry(workspace)
     if VERBOSE:
@@ -239,6 +267,7 @@ def user():
     if VERBOSE:
         print_json(user)
     click.echo(f"{user['id']}: {user['name']}")
+
 
 @click.command("projects", short_help="Show all projects")
 @click.option("-w", "--workspace", "workspace", type=str, default=None)
@@ -263,11 +292,17 @@ def workspaces():
             click.echo(f"{id}: {name}")
 
 
-@click.command("set_workspace", short_help="Set the default work space")
+@click.command("set_workspace", short_help="Set the default workspace")
 @click.argument("workspace", type=str)
 def s_workspace(workspace):
     name, wid = set_workspace(workspace)
     click.echo(f"Default workspace set to: {name}, {wid}")
+
+@click.command("set_project", short_help="Set the default project")
+@click.argument("project", type=str)
+def s_project(project):
+    name, pid = set_project(project)
+    click.echo(f"Default project set to: {name}, {pid}")
 
 
 @click.command("entries", short_help="Show previous 10 time entries")
@@ -310,7 +345,9 @@ def entries(workspace, info):
     help="Default is set with set_workspace",
 )
 @click.option("-d", "--description", type=str, default=None, help="Entry description")
-@click.option("-b", "--billable", is_flag=True, default=False, help="Set if entry is billable")
+@click.option(
+    "-b", "--billable", is_flag=True, default=False, help="Set if entry is billable"
+)
 @click.option("--project", "-p", default=None, help="Project ID")
 @click.option("--tag", "-t", multiple=True, help="Multiple tags permitted")
 @click.option("-i", "--info", "info", is_flag="True")
@@ -319,9 +356,9 @@ def add(workspace, duration, description, billable, project, tag, info):
     if status == 201:
         click.echo(f'Successfully added time entry with start {start.strftime("%d/%b/%Y")} and duration {duration}.')
         if info:
-            click.echo(f'User: {CONFIG["username"]}, Workspace: {CONFIG["workspace"]}, Project: {CONFIG["project"]}.')
+            click.echo( f'User: {CONFIG["username"]}, Workspace: {CONFIG["workspace"]}, Project: {CONFIG["project"]}.')
     else:
-        click.echo(f'Failed time entry with status {status}')
+        click.echo(f"Failed time entry with status {status}")
     if VERBOSE:
         print_json(ret)
 
@@ -332,9 +369,19 @@ def add(workspace, duration, description, billable, project, tag, info):
 def remove_entry(timeentryid, workspace):
     ret = remove_time_entry(timeentryid, workspace)
     if ret.ok:
-        click.echo(f'Removed entry {timeentryid}')
+        click.echo(f"Removed entry {timeentryid}")
     else:
-        click.echo(f'Failed to remove entry {timeentryid} with status {ret.status_code}')
+        click.echo( f"Failed to remove entry {timeentryid} with status {ret.status_code}")
+
+
+@click.command("in-progress", short_help="Get timer in progress")
+@click.option("-w", "--workspace", "workspace", type=str, default=None)
+def in_progress(workspace):
+    ret = get_in_progress(workspace)
+    click.echo(f"Current time entry: {ret['id']} started {ret['timeInterval']['start']}")
+    if VERBOSE:
+        print_json(ret)
+
 
 cli.add_command(start)
 cli.add_command(finish)
@@ -342,9 +389,11 @@ cli.add_command(add)
 cli.add_command(projects)
 cli.add_command(workspaces)
 cli.add_command(s_workspace)
+cli.add_command(s_project)
 cli.add_command(entries)
 cli.add_command(remove_entry)
 cli.add_command(user)
+cli.add_command(in_progress)
 
 
 def main():
