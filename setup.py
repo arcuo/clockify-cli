3e84d074f9dc121da32c589416416f11f193bed0
Benjamin Zachariae <benjamin.zachariae@gmail.com>
2020-04-20 16:45:21 +0200

    updated requirements

--- setup.py
+++ setup.py
@@ -2,18 +2,18 @@ from setuptools import setup
 
 setup(
     name='clockify_cli',
-    version='0.10',
+    version='1.10',
     py_modules=['clockify_cli'],
-    author='Theodore Hu',
-    url='https://github.com/t5/clockify-cli',
+    author='Benjamin Zachariae',
+    url='https://github.com/arcuo/clockify-cli',
     install_requires=[
-        'click==6.7',
-        'certifi==2018.8.13',
+        'click==7.1.1',
+        'certifi==2020.4.5.1',
         'chardet==3.0.4',
-        'click==6.7',
-        'idna==2.7',
-        'requests>=2.20.0',
-        'urllib3==1.23',
+        'idna==2.9',
+        'requests==2.23.0',
+        'urllib3==1.25.9',
+        'pytz==2019.3',
     ],
     entry_points='''
         [console_scripts]
